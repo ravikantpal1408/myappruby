187139922ea3cb15b4ae4a1b0b8e8663bc6e0841
Ravi Kant Pal <rpal@akaruilabs.com>
2018-04-09 16:34:37 +0530

    Gmail implemented

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-ActionMailer::Base.default_content_type = "text/html"
+#ActionMailer::Base.default_content_type = "text/html"
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :test
   # host = 'rails-tutorial-c9-mhartl.c9.io'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
